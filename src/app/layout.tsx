1a43b6edbdb6df9fdbdf9ad5f3ae2fb5f9f75f37
Loen10 <logannance1@mail.weber.edu>
2025-04-12 12:26:37 -0600

    Change font to default geistsans

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${geistSans.className} ${geistMono.className} antialiased`}>
         <ThemeProvider theme={theme}>
           <CssBaseline />
           {children}
