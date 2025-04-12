4cd9d05d5c5ee1beb553419866e4aa4c53c112fb
Loen10 <logannance1@mail.weber.edu>
2025-04-12 13:43:33 -0600

    Remove base path

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/logannance.github.io' : '',
+  // basePath: process.env.NODE_ENV === 'production' ? '/logannance.github.io' : '',
   trailingSlash: true,
 }
 
