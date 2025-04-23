a6b9bb05aac77f3ceadf9a9991edd97e678cc22a
Loen10 <logannance1@mail.weber.edu>
2025-04-23 15:20:22 -0600

    add queen

--- src/ui/chess/chess.tsx
+++ src/ui/chess/chess.tsx
@@ -2,54 +2,17 @@
 
 import { Box, Grid } from "@mui/material";
 import { useState } from "react";
+import { Piece } from "./piece";
 
 const SquareColor = {
   Light: '#ebecd0',
   Dark: '#779556',
 } as const; type SquareColor = typeof SquareColor[keyof typeof SquareColor];
 
-export const PieceChar = {
-  WhiteKing: 9812,
-  WhiteQueen: 9813,
-  WhiteRook: 9814,
-  WhiteBishop: 9815,
-  WhiteKnight: 9816,
-  WhitePawn: 9817,
-  BlackKing: 9818,
-  BlackQueen: 9819,
-  BlackRook: 9820,
-  BlackBishop: 9821,
-  BlackKnight: 9822,
-  BlackPawn: 9823,
-} as const; export type PieceChar = typeof PieceChar[keyof typeof PieceChar];
-
 type Vec = {
   x: number,
   y: number
 }
-
-const Piece = {
-  WhiteKing: {
-    char: 9812,
-    moves: [
-      function* upOne() {
-        yield { x: 0, y: -1 };
-      },
-
-      function* rightOne() {
-        yield { x: 1, y: 0 };
-      },
-
-      function* downOne() {
-        yield { x: 0, y: 1 };
-      },
-
-      function* leftOne() {
-        yield { x: -1, y: 0 };
-      }
-    ]
-  }
-} as const; type Piece = typeof Piece[keyof typeof Piece];
 
 type Square = {
   color: SquareColor,
@@ -70,14 +33,20 @@ export default function Chess() {
         color: SquareColor.Light,
         piece: Piece.WhiteKing,
       },
-      ...Array.from({ length: 7 }, (_, i) => ({
-        color: i % 2 === 1 ? SquareColor.Light : SquareColor.Dark,
+      {
+        color: SquareColor.Dark,
+        piece: Piece.WhiteQueen,
+      },
+      ...Array.from({ length: 6 }, (_, i) => ({
+        color: i % 2 === 0 ? SquareColor.Light : SquareColor.Dark,
       }))
     ],
     
-    Array.from({ length: 8 }, (_, i) => ({
-      color: i % 2 === 1 ? SquareColor.Light : SquareColor.Dark,
-    }))
+    ...Array.from( {length: 7 }, (_, i) => (
+      Array.from({ length: 8 }, (_, j) => ({
+        color: (i + j) % 2 === 1 ? SquareColor.Light : SquareColor.Dark,
+      }))
+    ))
   ]);
 
   const [active, setActive] = useState<
@@ -111,56 +80,61 @@ export default function Chess() {
                 onClick={() => {
                   const decorate = (
                     vecSqr: { sqr: Square } & Vec,
-                    dot?: 'dot'
+                    remove?: true
                   ) => {
                     vecSqr.sqr.piece!.moves.forEach(move => {
                       for (const m of move()) {
                         const x = vecSqr.x + m.x;
                         const y = vecSqr.y + m.y;
                   
-                        if (x < 0 || y < 0 || x > 7 || y > 1) {
+                        if (x < 0 || y < 0 || x > 7 || y > 7) {
                           return;
                         }
                   
                         const dest = board[y][x];
-                  
-                        if (!dest.piece) {
-                          dest.decoration = dot;
-                          setBoard([...board]);
+                        
+                        if (remove) {
+                          dest.decoration = undefined;
+                        } else {
+                          dest.decoration = dest.piece ? 'circle' : 'dot';
                         }
                       }
+
+                      setBoard([...board]);
                     });
                   };
+
+                  const capture = (active: { sqr: Square } & Vec) => {
+                    console.log('yo');
+                    square.decoration = undefined;
+                    square.piece = active.sqr.piece;
+                    decorate(active, true);
+                    active.sqr.piece = undefined;
+                    setActive(null);
+                  }
 
                   if (square.piece) {
                     // if the square has a piece and a decoration,
                     // it is a circle
                     if (square.decoration) {
-                      square.decoration = undefined;
-                      square.piece = active!.sqr.piece;
-                      active!.sqr.piece = undefined;
-                      setActive(null);
+                      capture(active!);
                       return;
                     }
                   
-                  setActive({ sqr: square, x: j, y: i });
-                  decorate({ sqr: square, x: j, y: i }, 'dot');
-                  return;
-                }
+                    setActive({ sqr: square, x: j, y: i });
+                    decorate({ sqr: square, x: j, y: i });
+                    return;
+                  }
               
-                // If clicked on square with no piece and no decoration, an
-                // empty square was clicked and nothing should be done
-                if (!square.decoration) {
-                  return;
-                }
-                
-                // no piece, yes decoration
-                square.decoration = undefined;
-                square.piece = active!.sqr.piece!;
-                decorate(active!);
-                active!.sqr.piece = undefined;
-                setActive(null);
-              }}
+                  // If clicked on square with no piece and no decoration, an
+                  // empty square was clicked and nothing should be done
+                  if (!square.decoration) {
+                    return;
+                  }
+                  
+                  // no piece, yes decoration
+                  capture(active!);
+                }}
               >
                 {square.decoration === 'dot' ? (
                   <Box
---
+++ src/ui/chess/piece.ts
@@ -0,0 +1,106 @@
+export const PieceChar = {
+  WhiteKing: 9812,
+  WhiteQueen: 9813,
+  WhiteRook: 9814,
+  WhiteBishop: 9815,
+  WhiteKnight: 9816,
+  WhitePawn: 9817,
+  BlackKing: 9818,
+  BlackQueen: 9819,
+  BlackRook: 9820,
+  BlackBishop: 9821,
+  BlackKnight: 9822,
+  BlackPawn: 9823,
+} as const; export type PieceChar = typeof PieceChar[keyof typeof PieceChar];
+
+export const Piece = {
+  WhiteKing: {
+    char: 9812,
+    moves: [
+      function* upOne() {
+        yield { x: 0, y: -1 };
+      },
+
+      function* upRightOne() {
+        yield { x: 1, y: -1 };
+      },
+
+      function* rightOne() {
+        yield { x: 1, y: 0 };
+      },
+
+      function* downRightOne() {
+        yield { x: 1, y: 1 };
+      },
+
+      function* downOne() {
+        yield { x: 0, y: 1 };
+      },
+
+      function* downLeftOne() {
+        yield { x: -1, y: 1 };
+      },
+
+      function* leftOne() {
+        yield { x: -1, y: 0 };
+      },
+
+      function* upLeftOne() {
+        yield { x: -1, y: -1 };
+      }
+    ]
+  },
+
+  WhiteQueen: {
+    char: 9813,
+    moves: [
+      function* upSlide() {
+        for (let y = -1; y > -8; --y) {
+          yield { x: 0, y: y };
+        }
+      },
+
+      function* upRightSlide() {
+        for (let vec = { x: 1, y: -1 }; vec.x < 8; ++vec.x, --vec.y) {
+          yield vec;
+        }
+      },
+
+      function* rightSlide() {
+        for (let x = 1; x < 8; ++x) {
+          yield { x: x, y: 0 };
+        }
+      },
+
+      function* downRightSlide() {
+        for (let vec = { x: 1, y: 1 }; vec.x < 8; ++vec.x, ++vec.y) {
+          yield vec;
+        }
+      },
+
+      function* downSlide() {
+        for (let y = 1; y < 8; ++y) {
+          yield { x: 0, y: y };
+        }
+      },
+
+      function* downLeftSlide() {
+        for (let vec = { x: -1, y: 1 }; vec.y < 8; --vec.x, ++vec.y) {
+          yield vec;
+        }
+      },
+
+      function* leftSlide() {
+        for (let x = -1; x > -8; --x) {
+          yield { x: x, y: 0 };
+        }
+      },
+
+      function* upLeftSlide() {
+        for (let vec = { x: -1, y: -1 }; vec.x > -8; --vec.x, --vec.y) {
+          yield vec;
+        }
+      },
+    ]
+  }
+} as const; export type Piece = typeof Piece[keyof typeof Piece];
